07b63aca3cd335aad2c706342a8766d8f234f05a
MetehanSimsek <metehan.simsek@outlook.com>
2023-09-08 17:01:59 +0300

    C# Terminal App

---
+++ Terminal/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Terminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.textBoxReceiver = new System.Windows.Forms.TextBox();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.baudrate = new System.Windows.Forms.ComboBox();
+            this.parity = new System.Windows.Forms.ComboBox();
+            this.dataBits = new System.Windows.Forms.ComboBox();
+            this.stopBits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileReadBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(52, 12);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxPorts_SelectedIndexChanged);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(201, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(201, 50);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 3;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // textBoxReceiver
+            // 
+            this.textBoxReceiver.Location = new System.Drawing.Point(73, 79);
+            this.textBoxReceiver.MaximumSize = new System.Drawing.Size(400, 200);
+            this.textBoxReceiver.MinimumSize = new System.Drawing.Size(200, 200);
+            this.textBoxReceiver.Multiline = true;
+            this.textBoxReceiver.Name = "textBoxReceiver";
+            this.textBoxReceiver.Size = new System.Drawing.Size(373, 200);
+            this.textBoxReceiver.TabIndex = 4;
+            this.textBoxReceiver.TextChanged += new System.EventHandler(this.textBoxReceiver_TextChanged);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(73, 327);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(373, 20);
+            this.textBoxSend.TabIndex = 5;
+            this.textBoxSend.TextChanged += new System.EventHandler(this.textBoxSend_TextChanged);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(472, 327);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 20);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "TX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "RX";
+            // 
+            // baudrate
+            // 
+            this.baudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudrate.FormattingEnabled = true;
+            this.baudrate.Items.AddRange(new object[] {
+            "110",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "56000",
+            "57600",
+            "115200",
+            "23040"});
+            this.baudrate.Location = new System.Drawing.Point(510, 100);
+            this.baudrate.Name = "baudrate";
+            this.baudrate.Size = new System.Drawing.Size(99, 21);
+            this.baudrate.TabIndex = 9;
+            this.baudrate.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_2);
+            // 
+            // parity
+            // 
+            this.parity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.parity.FormattingEnabled = true;
+            this.parity.Items.AddRange(new object[] {
+            "Even",
+            "Mark",
+            "None",
+            "Odd",
+            "Space"});
+            this.parity.Location = new System.Drawing.Point(510, 175);
+            this.parity.Name = "parity";
+            this.parity.Size = new System.Drawing.Size(99, 21);
+            this.parity.TabIndex = 10;
+            this.parity.SelectedIndexChanged += new System.EventHandler(this.parity_SelectedIndexChanged);
+            // 
+            // dataBits
+            // 
+            this.dataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dataBits.FormattingEnabled = true;
+            this.dataBits.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.dataBits.Location = new System.Drawing.Point(510, 136);
+            this.dataBits.Name = "dataBits";
+            this.dataBits.Size = new System.Drawing.Size(99, 21);
+            this.dataBits.TabIndex = 11;
+            this.dataBits.SelectedIndexChanged += new System.EventHandler(this.dataBits_SelectedIndexChanged);
+            // 
+            // stopBits
+            // 
+            this.stopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stopBits.FormattingEnabled = true;
+            this.stopBits.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.stopBits.Location = new System.Drawing.Point(510, 212);
+            this.stopBits.Name = "stopBits";
+            this.stopBits.Size = new System.Drawing.Size(99, 21);
+            this.stopBits.TabIndex = 12;
+            this.stopBits.SelectedIndexChanged += new System.EventHandler(this.stopBits_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(451, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Baudrate:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(469, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Parity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(451, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Data Bits:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(452, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Stop Bits:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 377);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "File Read";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "FILE SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileReadBox
+            // 
+            this.fileReadBox.Location = new System.Drawing.Point(73, 353);
+            this.fileReadBox.MaximumSize = new System.Drawing.Size(400, 200);
+            this.fileReadBox.MinimumSize = new System.Drawing.Size(200, 200);
+            this.fileReadBox.Multiline = true;
+            this.fileReadBox.Name = "fileReadBox";
+            this.fileReadBox.Size = new System.Drawing.Size(382, 200);
+            this.fileReadBox.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 556);
+            this.Controls.Add(this.fileReadBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stopBits);
+            this.Controls.Add(this.dataBits);
+            this.Controls.Add(this.parity);
+            this.Controls.Add(this.baudrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.textBoxReceiver);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.TextBox textBoxReceiver;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox baudrate;
+        private System.Windows.Forms.ComboBox parity;
+        private System.Windows.Forms.ComboBox dataBits;
+        private System.Windows.Forms.ComboBox stopBits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fileReadBox;
+    }
+}
+
---
+++ Terminal/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace Terminal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonDisconnect.Enabled = false;
+            buttonSend.Enabled = false;
+            foreach (var seriPort in SerialPort.GetPortNames())
+            {
+                comboBoxPorts.Items.Add(seriPort);
+            }
+            comboBoxPorts.SelectedIndex = 0;
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxPorts.Text;
+            try
+            {
+                serialPort1.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection Failed \n Hata:{ex.Message}", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (serialPort1.IsOpen)
+            {
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+                buttonSend.Enabled = true;
+            }
+
+
+
+        }
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            // serialPort1.PortName = comboBoxPorts.Text;
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                buttonSend.Enabled = false;
+
+            }
+        }
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(textBoxSend.Text);
+                textBoxSend.Clear();
+            }
+        }
+
+        public delegate void veriGoster(String s);
+
+        public void textBoxYaz(String s)
+        {
+            textBoxReceiver.Text += s;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String gelenVeri = serialPort1.ReadExisting();
+            //textBoxReceiver.Text += gelenVeri;
+            textBoxReceiver.Invoke(new veriGoster(textBoxYaz), gelenVeri);
+
+
+        }
+
+       
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            serialPort1.PortName = comboBoxPorts.Text;
+        }
+
+
+
+        private void textBoxReceiver_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSend_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_2(object sender, EventArgs e)
+        {
+            int baud = Int32.Parse(baudrate.Text);
+            serialPort1.BaudRate = baud;
+            comboBoxPorts.SelectedIndex = 0;
+        }
+
+        private void parity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (parity.Text == "Even")
+            {
+                serialPort1.Parity = Parity.Even;
+            }
+            if (parity.Text == "Mark")
+            {
+                serialPort1.Parity = Parity.Mark;
+            }
+            if (parity.Text == "None")
+            {
+                serialPort1.Parity = Parity.None;
+            }
+            if (parity.Text == "Odd")
+            {
+                serialPort1.Parity = Parity.Odd;
+            }
+            if (parity.Text == "Space")
+            {
+                serialPort1.Parity = Parity.Space;
+            }
+        }
+
+        private void dataBits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int dataBit = Int32.Parse(dataBits.Text);
+            serialPort1.DataBits = dataBit;
+           
+        }
+
+        private void stopBits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(stopBits.Text == "1")
+            {
+                serialPort1.StopBits = StopBits.One;
+            }
+            if (stopBits.Text == "1.5")
+            {
+                serialPort1.StopBits = StopBits.OnePointFive;
+            }
+                if (stopBits.Text == "2")
+            {
+                serialPort1.StopBits = StopBits.Two;
+            }
+            
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        public  void dosyadan_oku()
+        {
+ 
+        }
+
+        public static void dosyayaz()
+        {
+            string dosya_yolu = @"C:\Users\meteh\OneDrive\Masaüstü\ReadData.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            //sw.WriteLine("Merhaba");
+            //sw.WriteLine("Dunya");
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dosya_yolu = @"SC:\Users\meteh\OneDrive\Masaüstü\ReadData.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open,FileAccess.Read  );
+            StreamReader sw = new StreamReader(fs);
+            fileReadBox.Text = sw.ReadToEnd();
+
+            sw.Close();
+            fs.Close();
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(fileReadBox.Text);
+               
+            }
+        }
+
+        private void fileReadBox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
